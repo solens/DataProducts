d35157e4d1df6dc43b46a7cb8794edd31361fa2d
solens <s.solen@hotmail.com>
2016-02-28 22:08:19 -0500

    title changed in app

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(ggplot2)
 shinyUI(pageWithSidebar(
-  headerPanel("Hello Shiny!"),
+  headerPanel("Enter the number of carat and the quality of the cut to estimate the price of a diamond."),
   sidebarPanel(
     sliderInput(inputId="carat", label = "Number of carats",min=0.5,max=6,step=0.1,value=3),
     selectInput(inputId="cut",label = "Quality of the Cut", choices=c("Fair"=1,"Good"=2,"Very Good"=3,"Premium"=4,"Ideal"=5))
